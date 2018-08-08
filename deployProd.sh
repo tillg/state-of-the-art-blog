b0db7b0885a258a465857522cb2b4bf6e1620b2e
Till Gartner <till.gartner@gmail.com>
2018-08-08 14:08:24 +0700

    Yet another deploy fix.

--- deployProd.sh
+++ deployProd.sh
@@ -17,7 +17,7 @@ git config user.email "till.gartner@gmail.com"
 echo "Setting upstream and branch..."
 git remote add upstream "https://$GITHUB_TOKEN@github.com/tillg/state-of-the-art-blog.software"
 git fetch upstream
-git reset upstream/master
+git reset upstream/gh-pages
 
 echo "state-of-the-art-blog.software" > CNAME
 
@@ -26,6 +26,6 @@ touch .
 echo "Git add, commit and pushing..."
 git add -A .
 git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:master
+git push -q upstream HEAD:gh-pages
 
 echo "Gitted on state-of-the-art-blog.software"
